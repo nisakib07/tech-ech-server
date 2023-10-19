276bb620c63e0ac636915a610b39efc75e4b2c63
nisakib07 <websakib07@gmail.com>
2023-10-19 23:33:12 +0600

    single data read

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -28,6 +28,14 @@ async function run() {
     app.get("/products", async (req, res) => {
       const cursor = products.find();
       const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    app.get("/products/:id", async (req, res) => {
+      const id = req.params.id;
+      console.log(id);
+      const query = { _id: new ObjectId(id) };
+      const result = await products.findOne(query);
       res.send(result);
     });
 
